d8e7fa52800b1436126bead9d9793fcb16199c48
Raifa509 <fathimathulraifanp@gmail.com>
2025-09-19 21:05:30 +0530

    backend url updated

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://chatapp-backend-1-24i9.onrender.com", {
   transports: ["websocket", "polling"],
 });
